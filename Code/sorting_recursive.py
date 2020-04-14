dbd2fb7b1af865caa778d5ae358017547d07f75e
TasfiaAddrita <tasfia.addrita@gmail.com>
2020-04-13 23:47:32 -0700

    compare mergesort and quicksort and annotated algorithms

--- Code/sorting_recursive.py
+++ Code/sorting_recursive.py
@@ -1,6 +1,5 @@
 #!python
 
-# from sorting import random_ints
 from sorting_iterative import is_sorted, bubble_sort, selection_sort, insertion_sort
 from timeit import default_timer as timer
 
@@ -11,25 +10,14 @@ def random_ints(count=20, min=1, max=50):
     import random
     return [random.randint(min, max) for _ in range(count)]
 
-def time_it(func):
-    # Made wth love by Ben <3 - DS2.3
-    def wrapper(*args, **kwargs):
-        start = time.time()
-        result = func(*args, **kwargs)
-        end = time.time()
-        print(func.__name__ + ' took ' + str((end - start)) + '  s')
-        return result
-
-    return wrapper
-
 def merge(items1, items2):
     """
     Merge given lists of items, each assumed to already be in sorted order,
     and return a new list containing all items in sorted order.
         
-        Running time: O(n * log(n))
+        Running time: O(n + m)
         
-        Memory usage: O(n * log(n))
+        Memory usage: O(n + m)
     """
     # Repeat until one list is empty
     # Find minimum item in both lists and append it to new list
@@ -70,9 +58,11 @@ def split_sort_merge(items):
     sorting each with an iterative sorting algorithm, and merging results into
     a list in sorted order.
     
-        Running time: ??? Why and under what conditions?
+        Running time: 
+            Best: O(n)
+            Worst: O(n)
         
-        Memory usage: ??? Why and under what conditions?
+        Memory usage: O(n)
     """
     # Split items list into approximately equal halves
     # Sort each half using any other sorting algorithm
@@ -92,22 +82,17 @@ def merge_sort(items):
     Sort given items by splitting list into two approximately equal halves,
     sorting each recursively, and merging results into a list in sorted order.
         
-        Running time: ??? Why and under what conditions?
+        Running time:
+            Best: O(n * log(n))
+            Average: O(n * log(n))
+            Worst: O(n * log(n))
         
-        Memory usage: ??? Why and under what conditions?
+        Memory usage: O(log(n))
     """
     # Check if list is so small it's already sorted (base case)
     # Split items list into approximately equal halves
     # Sort each half by recursively calling merge sort
     # Merge sorted halves into one list in sorted order
-
-    # --- returns a new array of sorted elements --- 
-    # if len(items) <= 1:
-    #     return items
-    # mid = len(items)//2
-    # left_half = merge_sort(items[0:mid])
-    # right_half = merge_sort(items[mid:len(items)])
-    # return merge(left_half, right_half)
 
     # returns in-place array for sorted elements
     # --- thanks Ben and Shash --- 
@@ -120,13 +105,13 @@ def merge_sort(items):
         merged = merge(left, right)
         items[:] = merged # overwriting items array
 
-# def merge_vs_iterative(lst):
-#     merge_time = timeit.timeit('merge_sort(lst)', 'from __main__ import merge_sort, lst')
-#     insertion_time = timeit.timeit('insertion_sort(lst)')
-#     size = 'small list' if len(lst) <= 1000 else 'large list'
-
-#     return size, 'merge' if merge_time > insertion_time else 'insertion'
-
+def merge_sort_out_of_place(items):
+    if len(items) <= 1:
+        return items
+    mid = len(items)//2
+    left_half = merge_sort_out_of_place(items[0:mid])
+    right_half = merge_sort_out_of_place(items[mid:len(items)])
+    return merge(left_half, right_half)
 
 def partition(items, low, high):
     """
@@ -135,9 +120,12 @@ def partition(items, low, high):
     that range, moving pivot into index `p`, items less than pivot into range
     `[low...p-1]`, and items greater than pivot into range `[p+1...high]`.
         
-        Running time: ??? Why and under what conditions?
+        Running time: 
+            Best: O(n)
+            Average: O(n)
+            Worst: (n)
         
-        Memory usage: ??? Why and under what conditions?
+        Memory usage: O(1)
     """
     # Choose a pivot any way and document your method in docstring above
     divider = low 
@@ -154,16 +142,17 @@ def partition(items, low, high):
     items[pivot], items[divider] = items[divider], items[pivot]
     return divider
 
-
 def quick_sort(items, low=None, high=None):
     """
     Sort given items in place by partitioning items in range `[low...high]`
     around a pivot item and recursively sorting each remaining sublist range.
-        Best case running time: ??? Why and under what conditions?
+
+        Running time: 
+            Best: O(n * log(n))
+            Average: O(n * log(n))
+            Worst: O(n^2) -- almost reversed or sorted
         
-        Worst case running time: ??? Why and under what conditions?
-        
-        Memory usage: ??? Why and under what conditions?
+        Memory usage: O(log(n))
     """
     # Check if high and low range bounds have default values (not given)
     if low is None or high is None:
@@ -177,29 +166,47 @@ def quick_sort(items, low=None, high=None):
         quick_sort(items, low, pivot - 1)
         quick_sort(items, pivot + 1, high)
 
+
+def time(func, lst):
+    start = timer()
+    func(lst)
+    end = timer()
+    total = round((end - start) * 1000, 5)
+    # print(func, total)
+    return total
+
+def compare(num_comparisons, num_ints, func1, func2):
+    sort1_count = 0
+
+    for i in range(num_comparisons):
+        lst = random_ints(num_ints, 1, 100)
+        sort1 = time(merge_sort, lst)
+        sort2 = time(quick_sort, lst)
+
+        if sort1 < sort2:
+            sort1_count += 1
+
+    print(func1, sort1_count / num_comparisons)
+    print(func2, (num_comparisons - sort1_count) / num_comparisons)
+
 if __name__ == "__main__":
     # a = [9, 8, 7, 6, 5, 4, 3, 2, 1]
     # b = random_ints(50, 1, 30)
     # c = random_ints(1000, 1, 100)
-    # d = random_ints(1000000, 1, 1000)
+    # d = random_ints(10000, 1, 1000)
     # e = random_ints(15, 1, 50)
     # f = [4, 5, 7, 3, 1, 9, 6, 5]
+    # g = [41, 35, 9, 40, 18, 21, 29, 20, 35, 12, 46, 12, 3, 28, 40] # dups 12, 35, 40
 
-    # start = timer()
-    # merge_sort(d)
-    # end = timer()
-    # print('merge', round((end - start) * 1000, 5))
+    # lst = b
 
-    # start = timer()
-    # insertion_sort(d)
-    # end = timer()
-    # print('insertion', round((end - start) * 1000, 5))
+    # time(merge_sort_out_of_place, lst)
+    # time(insertion_sort, lst)
 
-    # print(f, 'original')
-    # print(partition(f, 0, len(f)))
-    # print(f, 'partioned')
-
-    lst = [41, 35, 9, 40, 18, 21, 29, 20, 35, 12, 46, 12, 3, 28, 40] # dups 12, 35, 40
-    print(lst)
-    quick_sort(lst)
-    print(lst)
+    # time(merge_sort, lst)
+    # time(quick_sort, lst)
+    
+    # compare(10000, 50, merge_sort_out_of_place, insertion_sort)
+    # compare(10000, 100, merge_sort, merge_sort_out_of_place)
+    compare(100, 1000, merge_sort, quick_sort)
+    
