dc7e1ffc943bcc47820f2544c98bf8282fad0041
TasfiaAddrita <tasfia.addrita@gmail.com>
2020-04-22 22:27:13 -0700

    completed required prefix challenges

--- Code/prefixtree.py
+++ Code/prefixtree.py
@@ -35,15 +35,28 @@ class PrefixTree:
 
     def is_empty(self):
         """Return True if this prefix tree is empty (contains no strings)."""
-        # TODO
+        return self.size == 0
 
+    # look at how to use _find_node to complete this method
     def contains(self, string):
         """Return True if this prefix tree contains the given string."""
-        # TODO
+        node = self.root
+        for char in string:
+            if not node.has_child(char):
+                return False
+            node = node.get_child(char)
+        return node.is_terminal()
 
     def insert(self, string):
         """Insert the given string into this prefix tree."""
-        # TODO
+        node = self.root
+        for char in string:
+            if not node.has_child(char):
+                node.add_child(char, PrefixTreeNode(char))
+            node = node.get_child(char)
+        if not node.is_terminal():
+            node.terminal = True
+            self.size += 1
 
     def _find_node(self, string):
         """Return a pair containing the deepest node in this prefix tree that
@@ -51,30 +64,46 @@ class PrefixTree:
         The depth returned is equal to the number of prefix characters matched.
         Search is done iteratively with a loop starting from the root node."""
         # Match the empty string
-        if len(string) == 0:
-            return self.root, 0
         # Start with the root node
         node = self.root
-        # TODO
-
+        depth = 0
+        for char in string:
+            if node.has_child(char):
+                node = node.get_child(char)
+                depth += 1
+        return node, depth
+        
+    # use _traverse
     def complete(self, prefix):
         """Return a list of all strings stored in this prefix tree that start
         with the given prefix string."""
         # Create a list of completions in prefix tree
         completions = []
-        # TODO
+        node, depth = self._find_node(prefix)
+        print(node, depth)
+        if depth == 0:
+            return []
+        self._traverse(node, prefix, completions.append)
+        return completions
 
+    # use _traverse
     def strings(self):
         """Return a list of all strings stored in this prefix tree."""
         # Create a list of all strings in prefix tree
         all_strings = []
-        # TODO
+        self._traverse(self.root, "", all_strings.append)
+        return all_strings
 
     def _traverse(self, node, prefix, visit):
         """Traverse this prefix tree with recursive depth-first traversal.
         Start at the given node with the given prefix representing its path in
         this prefix tree and visit each node with the given visit function."""
-        # TODO
+        if node.is_terminal():
+            visit(prefix)
+        
+        for char in node.children:
+            child = node.get_child(char)
+            self._traverse(child, prefix + char, visit)
 
 
 def create_prefix_tree(strings):
--- Code/sorting_integer.py
+++ Code/sorting_integer.py
@@ -1,5 +1,6 @@
 #!python
 
+from sorting_iterative import insertion_sort
 
 def counting_sort(numbers):
     """
@@ -34,15 +35,42 @@ def counting_sort(numbers):
 
 
 def bucket_sort(numbers, num_buckets=10):
+# def bucket_sort(numbers, num_buckets=None):
     """Sort given numbers by distributing into buckets representing subranges,
     then sorting each bucket and concatenating all buckets in sorted order.
     TODO: Running time: ??? Why and under what conditions?
     TODO: Memory usage: ??? Why and under what conditions?"""
-    # TODO: Find range of given numbers (minimum and maximum values)
-    # TODO: Create list of buckets to store numbers in subranges of input range
-    # TODO: Loop over given numbers and place each item in appropriate bucket
-    # TODO: Sort each bucket using any sorting algorithm (recursive or another)
-    # TODO: Loop over buckets and append each bucket's numbers into output list
+
+    # Find range of given numbers (minimum and maximum values)
+    # min_, max_ = min(numbers), max(numbers)
+    range_ = max(numbers) - min(numbers)
+    print(range_)
+    print(len(numbers))
+
+    # if num_buckets is None:
+    #     num_buckets = ??? # consider what's the max number of elements a bucket can have
+    
+    print(num_buckets)
+
+    # Create list of buckets to store numbers in subranges of input range
+    buckets = [[] for _ in range(num_buckets)]
+
+    # Loop over given numbers and place each item in appropriate bucket
+    for num in numbers:
+        percent = num / range_
+        i = range_ // num_buckets
+        # i = num // num_buckets # FIX THIS
+        buckets[i].append(num)
+    
+    # Sort each bucket using any sorting algorithm (recursive or another)
+    start = 0
+    for bucket in buckets:
+        insertion_sort(bucket)
+        end = start + len(bucket)
+        numbers[start:end] = bucket
+        start = end
+    
+    # Loop over buckets and append each bucket's numbers into output list
     # FIXME: Improve this to mutate input instead of creating new output list
 
 
@@ -50,8 +78,11 @@ if __name__ == "__main__":
         
     lst = [9, 8, 7, 5, 8, 3, 1, 6, 3, 8, 0, 2, 1, 5, 7, 3, 2]
     lst = [48, 46, 31, 6, 9, 28, 3, 10, 48, 19, 39, 50, 13, 9, 29, 50, 30, 12, 30, 22]
+    lst = list(range(0, 101))
 
     print(lst)
     print()
-    print(counting_sort(lst))
+    # print(counting_sort(lst))
+    print(bucket_sort(lst))
     print(lst) # [0, 1, 1, 2, 2, 3, 3, 3, 5, 5, 6, 7, 7, 8, 8, 8, 9]
+
